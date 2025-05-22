8725615effd208f15195e3f811db1d70568a799c
keimel <comeau9006@gmail.com>
2025-05-22 23:34:37 +0200

    SpeedEffect implementiert.
    devDungeon SpeedEffect für ItemPotionSpeedPotion Aufgabe
    implementiert.

--- devDungeon/src/item/effects/SpeedEffect.java
+++ devDungeon/src/item/effects/SpeedEffect.java
@@ -1,6 +1,8 @@
 package item.effects;
 
 import core.Entity;
+import core.components.VelocityComponent;
+import core.utils.components.MissingComponentException;
 import systems.EventScheduler;
 
 /**
@@ -36,6 +38,21 @@ public class SpeedEffect {
    * @param target The entity to which the speed effect will be applied.
    */
   public void applySpeedEffect(Entity target) {
-    throw new UnsupportedOperationException("Method not implemented.");
+      //liefert Geschwindigkeitskomponente, wenn diese Vorhanden ist
+      VelocityComponent velocityComponent = target
+          .fetch(VelocityComponent.class)
+          .orElseThrow( () -> MissingComponentException.build(target, VelocityComponent.class));
+
+      //standart Geschwindigkeit für x und y Bewegung
+      float normalX = velocityComponent.xVelocity();
+      float normalY = velocityComponent.yVelocity();
+
+      //schreibt Entität neue Geschwindigkeit zu
+      target.add(new VelocityComponent(normalX * speedIncrease, normalY * speedIncrease));
+
+      //nach buff Ablauf wird Geschwindigkeit auf Normalwerte zurückgesetzt
+      EVENT_SCHEDULER.scheduleAction(
+          () -> target.add(new VelocityComponent(normalX, normalY)),
+          1000L * duration);
   }
 }
